fcf75ab6a91a11414718fc00a8daaf6aa85a5def
Aleksand Kiselev <leksandr777kiselev@yandex.ru>
2026-05-03 21:22:56 +0300

    Исправлены замечания в  task4 (#1)

--- task_4.py
+++ task_4.py
@@ -1,36 +1,39 @@
-#Мог неверно понять подсказку, но кажется подсказка в задании должна быть с точностью до наоборот
-#Метод get_hours() должен вернуть cls: return cls(name, hours, rest_days, email). Метод get_email — аналогично. Методу set_hourly_payment() ничего возвращать не нужно, потому что он только меняет значение переменной.
-# метод set_hourly_payment как раз воздействует на переменную класса, принемую ко всем объектам, поэтому должен быть cls
-
 class EmployeeSalary:
     hourly_payment = 400
-    def __init__(self,name,hours,rest_days,email):
+    default_domain = "email.com"  
+
+    def __init__(self, name, hours, rest_days, email):
         self.name = name
         self.hours = hours
         self.rest_days = rest_days
         self.email = email
-    
-    def get_hours(self):
-        if self.hours is None:
-            return (7 - self.rest_days) * 8
-        else:
-            return self.hours
-    
-    def get_email(self):
-        if self.email is None:
-            return f"{self.name}@email.com"
-        else:
-            return self.email
-            
 
     @classmethod
-    def set_hourly_payment(cls,new_hourly_payment):
+    def calculate_hours(cls, rest_days):
+        return (7 - rest_days) * 8
+
+    @classmethod
+    def generate_email(cls, name):
+        return f"{name}@{cls.default_domain}"
+
+    def get_hours(self):
+        if self.hours is None:
+            return self.calculate_hours(self.rest_days)
+        return self.hours
+
+    def get_email(self):
+        if self.email is None:
+            return self.generate_email(self.name)
+        return self.email
+
+    @classmethod
+    def set_hourly_payment(cls, new_hourly_payment):
         cls.hourly_payment = new_hourly_payment
 
     def salary(self):
-        return self.get_hours()* self.hourly_payment
+        return self.get_hours() * self.hourly_payment
 
-#пример    
+
 employee = EmployeeSalary(
     name="Ivanov",
     hours=None,
@@ -40,5 +43,7 @@ employee = EmployeeSalary(
 
 print(f"Имя: {employee.name}")
 print(f"Часы: {employee.get_hours()}")
-print(f"Email: {employee.get_email()}")
+print(f"Email: {employee.get_email()}") 
 print(f"Зарплата: {employee.salary()} руб.")
+
+
